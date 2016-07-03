5ce74c3788bd7afcf3e33bfa1306132a74465cb2
haokuan <jingdaohao@gmail.com>
2016-07-03 10:53:33 +0800

    fix bug

--- gds/blueprint/api/task.py
+++ gds/blueprint/api/task.py
@@ -33,7 +33,7 @@ def task(tid=None):
         result = json.dumps({'stat':1, 'desc':'Task is set successfully.', 'task':{'_id':tid}}, ensure_ascii=False, sort_keys=True, indent=4).encode('utf8')
     else:
         if limit == 'one':
-            result = Task.queryOne(user, condition, projection=projection)
+            result = Task.queryOne(user, condition, projection=projection, limit=None)
         else:
             result = list(Task.queryAll(user, condition, projection=projection))
         result = json.dumps({'stat':1, 'desc':'', 'task':result}, ensure_ascii=False, sort_keys=True, indent=4).encode('utf8')
